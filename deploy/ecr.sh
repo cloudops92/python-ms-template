2b0af5b221514c780eb3c1c520ae42b678102492
mishal <mishalshah92@gmail.com>
2019-05-12 13:02:14 +0530

    Reading aws region from env.

--- deploy/ecr.sh
+++ deploy/ecr.sh
@@ -11,5 +11,5 @@ aws_account=$(aws sts get-caller-identity --query 'Account' --output text)
 }
 
 docker tag ${repository_name}:latest ${aws_account}.dkr.ecr.us-east-1.amazonaws.com/${repository_name}:${tag}
-$(aws ecr get-login --region ${aws_region})
+$(aws ecr get-login --no-include-email --region ${aws_region})
 docker push ${aws_account}.dkr.ecr.${aws_region}.amazonaws.com/${repository_name}:${tag}
